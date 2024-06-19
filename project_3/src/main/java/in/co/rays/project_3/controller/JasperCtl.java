6a94d9353d62fb8900d0fc58b2c60b6382f4f93c
sonalipatel223 <148206199+sonalipatel223@users.noreply.github.com>
2024-06-19 15:24:10 +0530

    Update JasperCtl.java

--- project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
+++ project_3/src/main/java/in/co/rays/project_3/controller/JasperCtl.java
@@ -1,102 +1,102 @@
-package in.co.rays.project_3.controller;
+// package in.co.rays.project_3.controller;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+// import java.io.IOException;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.ResourceBundle;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+// import javax.servlet.ServletException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpSession;
 
-import org.hibernate.impl.SessionImpl;
+// import org.hibernate.impl.SessionImpl;
 
-import in.co.rays.project_3.dto.UserDTO;
-import in.co.rays.project_3.util.HibDataSource;
-import in.co.rays.project_3.util.JDBCDataSource;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
+// import in.co.rays.project_3.dto.UserDTO;
+// import in.co.rays.project_3.util.HibDataSource;
+// import in.co.rays.project_3.util.JDBCDataSource;
+// import net.sf.jasperreports.engine.JasperCompileManager;
+// import net.sf.jasperreports.engine.JasperExportManager;
+// import net.sf.jasperreports.engine.JasperFillManager;
+// import net.sf.jasperreports.engine.JasperPrint;
+// import net.sf.jasperreports.engine.JasperReport;
 
-/**
- * Jasper functionality Controller. Performs operation for Print pdf of
- * MarksheetMeriteList
- * @author Sonali Patel
- *
- */
-@WebServlet(name = "JasperCtl", urlPatterns = { "/ctl/JasperCtl" })
-public class JasperCtl extends BaseCtl {
+// /**
+//  * Jasper functionality Controller. Performs operation for Print pdf of
+//  * MarksheetMeriteList
+//  * @author Sonali Patel
+//  *
+//  */
+// @WebServlet(name = "JasperCtl", urlPatterns = { "/ctl/JasperCtl" })
+// public class JasperCtl extends BaseCtl {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+// 	/**
+// 	 * 
+// 	 */
+// 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
+// 	@Override
+// 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// 		try {
 			
-			ResourceBundle rb= ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
-			/* Compilation of jrxml file */
+// 			ResourceBundle rb= ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+// 			/* Compilation of jrxml file */
 			
-			/*jasper compilar manager */
-			JasperReport jasperReport = JasperCompileManager.compileReport(rb.getString("jasper"));
+// 			/*jasper compilar manager */
+// 			JasperReport jasperReport = JasperCompileManager.compileReport(rb.getString("jasper"));
 			
-			
-
-			HttpSession session = request.getSession(true);
-			UserDTO dto = (UserDTO) session.getAttribute("user");
-			dto.getFirstName();
-			dto.getLastName();
-
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("ID", 1l);
-			java.sql.Connection conn = null;
-
 			
 
+// 			HttpSession session = request.getSession(true);
+// 			UserDTO dto = (UserDTO) session.getAttribute("user");
+// 			dto.getFirstName();
+// 			dto.getLastName();
 
-			String Database = rb.getString("DATABASE");
+// 			Map<String, Object> map = new HashMap<String, Object>();
+// 			map.put("ID", 1l);
+// 			java.sql.Connection conn = null;
+
 			
-			if ("Hibernate".equalsIgnoreCase(Database)) {
-				conn = ((SessionImpl) HibDataSource.getSession()).connection();
-			}
 
-			if ("JDBC".equalsIgnoreCase(Database)) {
-				conn = JDBCDataSource.getConnection();
-			}
 
-			 /* Filling data into the report */
+// 			String Database = rb.getString("DATABASE");
 			
-			/*jasper file or print manager */
+// 			if ("Hibernate".equalsIgnoreCase(Database)) {
+// 				conn = ((SessionImpl) HibDataSource.getSession()).connection();
+// 			}
+
+// 			if ("JDBC".equalsIgnoreCase(Database)) {
+// 				conn = JDBCDataSource.getConnection();
+// 			}
+
+// 			 /* Filling data into the report */
 			
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, conn);
-
-			/* Export Jasper report */
+// 			/*jasper file or print manager */
 			
-			/*jasper export manager */
-			byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+// 			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, conn);
 
-			response.setContentType("application/pdf");
-			response.getOutputStream().write(pdf);
-			response.getOutputStream().flush();
-		} catch (Exception e) {
+// 			/* Export Jasper report */
+			
+// 			/*jasper export manager */
+// 			byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
 
-		}
-	}
+// 			response.setContentType("application/pdf");
+// 			response.getOutputStream().write(pdf);
+// 			response.getOutputStream().flush();
+// 		} catch (Exception e) {
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+// 		}
+// 	}
 
-	}
+// 	@Override
+// 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-	@Override
-	protected String getView() {
-		return null;
-	}
+// 	}
 
-}
+// 	@Override
+// 	protected String getView() {
+// 		return null;
+// 	}
+
+// }
